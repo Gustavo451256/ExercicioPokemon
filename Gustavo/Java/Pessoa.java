8d8ff6febacb8f1fa321d768403167ae2625a5a8
Gustavo451256 <141927038+Gustavo451256@users.noreply.github.com>
2023-10-03 12:10:17 -0300

    Add files via upload

---
+++ Gustavo/Java/Pessoa.java
@@ -0,0 +1,15 @@
+/*
+* Nome: Gustavo Toledo Machado
+* Data: 29/08/2023
+*/
+public class Pessoa {
+    private String nome;
+
+    public void setNome (String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
